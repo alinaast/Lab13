aed5e79bf3e27683996c72fe48be1379b461f828
alinaast <55184585+alinaast@users.noreply.github.com>
2019-11-14 13:04:20 +0300

    Add files via upload

---
+++ AAA2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int i;
+	int n = 5;
+	int m[5];
+	m[0] = 1;
+	for (i = 1; i <= n; i++)
+	{
+		m[i] = m[i - 1] + 2;
+		cout << m[i] << " ";
+	}
+	return 0;
+}
---
+++ AAA3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 10;
+	int m[10] = { 1, 3, 45, 6, 7, 5, 33, 65, 90, 2 };
+	int i, a, b, c;
+	a = m[0];
+	b = m[1];
+	c = a + b;
+	cout << c << " ";
+	for (i = 2; i <= n; i++) {
+		c = c + m[i];
+		m[i] = c;
+		cout << m[i] << " ";
+	}
+	return 0;
+}
