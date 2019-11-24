e99de2803ed7e2685778cb3d7fad080ba23ac52c
alinaast <55184585+alinaast@users.noreply.github.com>
2019-11-25 01:12:36 +0300

    Add files via upload

---
+++ 13.4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n, i, a;
+	cout << "Введите размер массива: ";
+	cin >> n;
+	if (n > 0)
+	{
+		int* m = new int[n];
+		for (i = 1; i <= n; i++)
+	{
+			cout << "Введите " << i << " элемент: ";
+		    cin >> m[i];
+		}
+		for (i = 1, a = n + 1; i <= n; i++, a--)
+		{
+			if (i % 2 == 1)
+				cout << m[i] << " ";
+			else
+				cout << m[a] << " ";
+		}
+	}
+	return 0;
+}
---
+++ 13.5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n, i, c;
+	cout << "Введите размер массива: ";
+	cin >> n;
+	if (n > 0)
+	{
+		int* m = new int[n];
+		for (i = 1; i <= n; i++)
+		{
+			cout << "Введите " << i << " элемент: ";
+			cin >> m[i];
+		}
+		for (i = 1; i <= n; i++)
+			if (i % 2 == 1)
+				cout << i << " элемент = " << m[i] << " ";
+		cout << endl;
+		for (i = n; i >= 1; i--)
+			if (i % 2 == 0)
+				cout << i << " элемент = " << m[i] << " ";
+	}
+	return 0;
+}
